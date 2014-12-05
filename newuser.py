be192d80f3e788ce91cecf4d705166e512f6415a
Liutongxin <liutongxinyy@vip.qq.com>
2014-12-05 18:23:21 +0800

    upload

---
+++ newuser.py
@@ -0,0 +1,11 @@
+from app import db,models
+u = models.User(nickname='john', email='john@email.com', role=models.ROLE_USER)
+db.session.add(u)
+db.session.commit()
+
+u = models.User(nickname='susan', email='susan@email.com', role=models.ROLE_USER)
+db.session.add(u)
+db.session.commit()
+
+users = models.User.query.all()
+print users
